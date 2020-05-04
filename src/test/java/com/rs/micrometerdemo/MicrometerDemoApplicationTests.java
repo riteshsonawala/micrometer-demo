19a6b9b3fdaeca9b0240334191c0903038392332
ritesh <ritesh.sonawala@gmail.com>
2020-05-04 09:47:01 +0100

    Init Add

---
+++ src/test/java/com/rs/micrometerdemo/MicrometerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.micrometerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
